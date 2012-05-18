dc29664e34bea5c0ec4c69d200b3dfd7abca6a2a
Daniel Yang <yangsunwoo@gmail.com>
2012-05-18 18:27:49 -0400

    added authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   helper_method :current_user
+  helper_method :logged_in?
          
 private
          
+  def logged_in?
+    if session[:user_id] == @user.id
+      @user
+    else
+      nil
+    end
+  end
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
