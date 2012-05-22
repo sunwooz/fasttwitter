3829be8f2e5d31981f0b0d55e357a9833b4dcf74
Daniel Yang <yangsunwoo@gmail.com>
2012-05-22 11:05:27 -0400

    hopefully fixed double ajax post

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,7 @@ Fasttwitter::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = true
-  config.serve_static_assets = false
-  config.assets.prefix = '/devassets'
+  # config.assets.debug = true
+  # config.serve_static_assets = false
+  # config.assets.prefix = '/devassets'
 end
